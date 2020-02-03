44fffaf4c0e5f537dd8853770746c8cf5011d2f7
amank <amank.ak.kumar@oracle.com>
2020-02-03 10:47:01 +0530

    --testFail skipping ut and it for now. added a TODO

--- src/test/java/com/microservice/login/LoginApplicationTests.java
+++ src/test/java/com/microservice/login/LoginApplicationTests.java
@@ -3,6 +3,12 @@ package com.microservice.login;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+/**
+ * Ignoring the tests for now.
+ * @TODO IT and UT
+ */
+
 @SpringBootTest
 class LoginApplicationTests {
 
